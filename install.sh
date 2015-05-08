a4b7aff2d737c3d29ecd8b18e5d552c7c646281d
GuillePolito <guillermopolito@gmail.com>
2015-05-08 17:27:24 +0200

    fixed install script

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 wget -O- https://raw.githubusercontent.com/pillar-markup/book-skeleton/master/download.sh | bash
-./pharo eval "Gofer it
+./pharo Pharo.image eval "Gofer it
 	smalltalkhubUser: 'StephaneDucasse' project: 'Ecstatic';
 	package: 'Ecstatic';
 	load" 
