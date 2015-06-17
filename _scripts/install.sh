40647bf4662b10cfdf7138229a37551e2825d5ca
GuillePolito <guillermopolito@gmail.com>
2015-06-17 16:03:29 +0200

    removed extra files

--- _scripts/install.sh
+++
@@ -1,3 +0,0 @@
-wget --quiet -O - get.pharo.org/40+vm | bash
-REPO=http://smalltalkhub.com/mc/StephaneDucasse/Ecstatic/main
-./pharo Pharo.image config $REPO ConfigurationOfEcstatic --install=development --group='ALL'
