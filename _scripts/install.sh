84a7784e7290f08d0c4dc8ef582e362bb479f31d
GuillePolito <guillermopolito@gmail.com>
2015-08-28 15:29:49 +0200

    depending in stable pillar

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -1,3 +1,3 @@
 wget --quiet -O - get.pharo.org/40+vm | bash
 REPO=http://smalltalkhub.com/mc/StephaneDucasse/Ecstatic/main
-./pharo Pharo.image config $REPO ConfigurationOfEcstatic --install=development --group='ALL'
+./pharo Pharo.image config $REPO ConfigurationOfEcstatic --install=0.5 --group='ALL'
