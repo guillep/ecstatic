22873460158d698cf379a34eb876306278220279
StephaneDucasse <stephane.ducasse@inria.fr>
2015-10-10 21:12:34 +0200

    loading latest version

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -1,3 +1,3 @@
 wget --quiet -O - get.pharo.org/40+vm | bash
 REPO=http://smalltalkhub.com/mc/StephaneDucasse/Ecstatic/main
-./pharo Pharo.image config $REPO ConfigurationOfEcstatic --install=0.5 --group='ALL'
+./pharo Pharo.image config $REPO ConfigurationOfEcstatic --install=0.6 --group='ALL'
