b9c64daf6545234b917a42c3bc558385ea41985a
Guille Polito <guille@manor.lifl.fr>
2016-03-05 09:23:56 +0100

    fixed download script

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -1,3 +1,3 @@
 wget --quiet -O - get.pharo.org/40+vm | bash
-REPO=http://smalltalkhub.com/mc/StephaneDucasse/Ecstatic/main
-./pharo Pharo.image config $REPO ConfigurationOfEcstatic --install=0.7 --group='ALL'
+REPO=http://smalltalkhub.com/mc/RMoD/Ecstatic/main
+./pharo Pharo.image config $REPO ConfigurationOfEcstatic --install=0.8 --group='ALL'
