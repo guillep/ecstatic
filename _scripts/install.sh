a99032adf1b926872e0a73afacd5f128630bac1e
tinchodias <tinchodias@gmail.com>
2016-04-23 15:10:19 -0300

    Upgrade dependency: Citezen 2.2 -> 2.6

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -1,3 +1,3 @@
 wget --quiet -O - get.pharo.org/40+vm | bash
 REPO=http://smalltalkhub.com/mc/RMoD/Ecstatic/main
-./pharo Pharo.image config $REPO ConfigurationOfEcstatic --install=0.8 --group='ALL'
+./pharo Pharo.image config $REPO ConfigurationOfEcstatic --install=0.9 --group='ALL'
