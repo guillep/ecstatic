f8016c8f03ddeaf76ec5fa9bb1ff239bba3362e9
CyrilFerlicot <cyril@ferlicot.me>
2018-07-16 11:11:57 +0200

    Add shebangs to scripts

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 wget --quiet -O - get.pharo.org/61+vm | bash
 REPO=http://smalltalkhub.com/mc/RMoD/Ecstatic/main
 ./pharo Pharo.image config $REPO ConfigurationOfEcstatic --install=0.9.1 --group='ALL'
